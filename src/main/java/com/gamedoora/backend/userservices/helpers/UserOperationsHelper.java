b227f09c527133eb5a4e4d5bfe813479488d1edc
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2023-06-24 11:18:30 +0530

    User operations stub

--- src/main/java/com/gamedoora/backend/userservices/helpers/UserOperationsHelper.java
+++ src/main/java/com/gamedoora/backend/userservices/helpers/UserOperationsHelper.java
@@ -1,4 +1,16 @@
 package com.gamedoora.backend.userservices.helpers;
 
+import com.gamedoora.backend.userservices.dto.UserDTO;
+import org.keycloak.admin.client.resource.UsersResource;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserOperationsHelper {
+    private UsersResource keyclockUsersResource;
+
+    public void addUser(UserDTO userDto){
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setFirstName(userDto.getFirstName());
+    }
 }
