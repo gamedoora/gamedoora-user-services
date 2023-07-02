d6f306654f6c2ee8516def6bd5067e87d5b8bf35
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2023-07-02 11:34:54 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-BINARY="gamedoora-user-services.jar"
-LOGGING="-Dlogging.path=/home/ec2/user-services/logs"
+BINARY="/home/ec2-user/server/user-services/gamedoora-user-services.jar"
+LOGGING="-Dlogging.path=//home/ec2-user/server/user-services/logs"
 PORT=8081
 java -Duser.timezone=UTC $LOGGING $JAVA_OPTS -Dserver.port=${PORT} -jar $BINARY
