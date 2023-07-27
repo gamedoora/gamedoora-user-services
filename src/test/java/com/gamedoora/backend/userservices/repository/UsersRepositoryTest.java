158f7c043ffe26f9b8b566b77a26a7fd80f76e55
picksitquick <picksitquick@gmail.com>
2023-07-27 18:59:21 +0530

    updated UserRepositoryTest and its configs

--- src/test/java/com/gamedoora/backend/userservices/repository/UsersRepositoryTest.java
+++ src/test/java/com/gamedoora/backend/userservices/repository/UsersRepositoryTest.java
@@ -1,46 +1,66 @@
 package com.gamedoora.backend.userservices.repository;
 
-import com.gamedoora.backend.userservices.config.PropertiesConfig;
+import com.gamedoora.model.dao.Roles;
+import com.gamedoora.model.dao.Skills;
+import com.gamedoora.model.dao.UserRole;
+import com.gamedoora.model.dao.UserSkills;
 import com.gamedoora.model.dao.Users;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-@DataJpaTest
-@EnableConfigurationProperties(PropertiesConfig.class)
-@TestPropertySource("classpath:test.properties")
+@DataJpaTest(properties = {"spring.cloud.config.enabled=false"})
 class UsersRepositoryTest {
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Autowired
-    private EntityManager entityManager;
 
     @Autowired
     private UsersRepository usersRepository;
 
+    private Users users = new Users();
+
+    @BeforeEach
+    void setup(){
+
+        users = Users.builder()
+                .id(1L)
+                .firstName("Test")
+                .email("test@gmail.com")
+                .providerToken("")//required fields while building objects
+                .build();
+    }
+
 
     @Test
     public void sampleTest(){
-        Users users = new Users();
-        users.setId(1);
 
         usersRepository.save(users);
 
         Optional<Users> dummy = usersRepository.findById(1L);
+        Users test = usersRepository.findByFirstName("ok");
 
+        List<Users> sample = usersRepository.findBySkills_Name("sample");
+
+        assertNotNull(sample);
         assertNotNull(dummy);
+        assertNull(test);
     }
+
+    @Test
+    void findByName() {
+        usersRepository.save(users);
+        Users userValue = usersRepository.findByFirstName(users.getFirstName());
+        assertNotNull(userValue);
+        assertEquals(userValue.getFirstName() , "Test");
+    }
+
 }
