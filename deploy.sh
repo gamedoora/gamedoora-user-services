a3ccc64720229e7352f2094903d7b85ec44661b8
picksitquick <picksitquick@gmail.com>
2023-09-02 15:41:34 +0530

    Changed the status code to 0, and updated env variable name

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo $SSH_USERNAME
 echo $SSH_HOST
 
 # connect to the Server instance and set up the remote directory
-ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASS@$SSH_HOST" "
+ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST" "
   if [ -d $REMOTE_DIRECTORY ]; then
     cd $REMOTE_DIRECTORY
     if [ -f $REMOTE_JAR_FILE ]; then
@@ -20,17 +20,17 @@ ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASS@$SSH_HOST" "
 
 #COPY SYSTEMD INTO LOCAL
 DEST_PATH="/usr/lib/systemd/system/"
-scp gamedoora-user-service.service -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASS@$SSH_HOST:$DEST_PATH"
+scp gamedoora-user-service.service -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST:$DEST_PATH"
 
 # Stop the service
-ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASS@$SSH_HOST" 'sudo systemctl daemon-reload; sudo systemctl stop gamedoora-user-services'
+ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST" 'sudo systemctl daemon-reload; sudo systemctl stop gamedoora-user-services'
 
 # copy the new JAR file to the remote directory and rename it
-scp $JAR_FILE -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASS@$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
-ssh $SSH_USERNAME:$SSH_PASS@host 'sudo chown gamedoora:gamedoora /opt/gamedoora/\*.jar; sudo systemctl restart gamedoora-user-services'
+scp $JAR_FILE -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
+ssh $SSH_USERNAME:$SSH_PASSWORD@host 'sudo chown gamedoora:gamedoora /opt/gamedoora/\*.jar; sudo systemctl restart gamedoora-user-services'
 
 # Check the exit status of the previous command and set an output variable accordingly
-if [ $? -eq 143 ]; then # changed success status
+if [ $? -eq 0 ]; then
   echo "::set-output name=status::success"
 else
   echo "::set-output name=status::failure"
