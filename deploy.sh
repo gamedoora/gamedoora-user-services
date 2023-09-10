2176e8614c7ac3de04da294bf4949bb6258116b1
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2023-09-10 19:06:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ echo $SSH_USERNAME
 echo $SSH_HOST
 
 # connect to the Server instance and set up the remote directory 
- sshpass -p $SSH_PASSWORD ssh -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
+ sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
   if [ -d $REMOTE_DIRECTORY ]; then
     cd $REMOTE_DIRECTORY
     if [ -f $REMOTE_JAR_FILE ]; then
@@ -21,14 +21,14 @@ echo $SSH_HOST
 #COPY SYSTEMD INTO LOCAL
 DEST_PATH="/usr/lib/systemd/system/"
 echo "Copying files"
-sshpass -p $SSH_PASSWORD scp gamedoora-user-service.service -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST:$DEST_PATH"
+sshpass -p $SSH_PASS scp gamedoora-user-service.service -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST:$DEST_PATH"
 echo "Restarting service"
 # Stop the service
-sshpass -p $SSH_PASSWORD ssh -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" 'sudo systemctl daemon-reload; sudo systemctl stop gamedoora-user-services'
+sshpass -p $SSH_PASS ssh -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" 'sudo systemctl daemon-reload; sudo systemctl stop gamedoora-user-services'
 
 # copy the new JAR file to the remote directory and rename it
-sshpass -p $SSH_PASSWORD scp $JAR_FILE -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
-sshpass -p $SSH_PASSWORD ssh $SSH_USERNAME@host 'sudo chown gamedoora:gamedoora /opt/gamedoora/\*.jar; sudo systemctl restart gamedoora-user-services'
+sshpass -p $SSH_PASS scp $JAR_FILE -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
+sshpass -p $SSH_PASS ssh $SSH_USERNAME@host 'sudo chown gamedoora:gamedoora /opt/gamedoora/\*.jar; sudo systemctl restart gamedoora-user-services'
 
 # Check the exit status of the previous command and set an output variable accordingly
 if [ $? -eq 0 ]; then
