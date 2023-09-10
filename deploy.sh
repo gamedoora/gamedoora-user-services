6835134f1c884fe46ef249a9f72a1608d218a960
A.P.Rajshekhar <ap.rajshekhar@gmail.com>
2023-09-10 18:42:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ REMOTE_JAR_FILE=gamedoora-user-services.jar
 echo $SSH_USERNAME
 echo $SSH_HOST
 
-# connect to the Server instance and set up the remote directory
-ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST" "
+# connect to the Server instance and set up the remote directory 
+ sshpass -p $SSH_PASSWORD ssh -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" "
   if [ -d $REMOTE_DIRECTORY ]; then
     cd $REMOTE_DIRECTORY
     if [ -f $REMOTE_JAR_FILE ]; then
@@ -20,14 +20,14 @@ ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST" "
 
 #COPY SYSTEMD INTO LOCAL
 DEST_PATH="/usr/lib/systemd/system/"
-scp gamedoora-user-service.service -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST:$DEST_PATH"
+sshpass -p $SSH_PASSWORD sshpass -p ${{ SSH_PASSWORD }} scp gamedoora-user-service.service -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST:$DEST_PATH"
 
 # Stop the service
-ssh -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST" 'sudo systemctl daemon-reload; sudo systemctl stop gamedoora-user-services'
+sshpass -p $SSH_PASSWORD ssh -o StrictHostKeyChecking=no "$SSH_USERNAME@$SSH_HOST" 'sudo systemctl daemon-reload; sudo systemctl stop gamedoora-user-services'
 
 # copy the new JAR file to the remote directory and rename it
-scp $JAR_FILE -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_PASSWORD@$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
-ssh $SSH_USERNAME:$SSH_PASSWORD@host 'sudo chown gamedoora:gamedoora /opt/gamedoora/\*.jar; sudo systemctl restart gamedoora-user-services'
+sshpass -p $SSH_PASSWORD scp $JAR_FILE -o StrictHostKeyChecking=no "$SSH_USERNAME:$SSH_HOST:$REMOTE_DIRECTORY/$REMOTE_JAR_FILE"
+sshpass -p $SSH_PASSWORD ssh $SSH_USERNAME@host 'sudo chown gamedoora:gamedoora /opt/gamedoora/\*.jar; sudo systemctl restart gamedoora-user-services'
 
 # Check the exit status of the previous command and set an output variable accordingly
 if [ $? -eq 0 ]; then
